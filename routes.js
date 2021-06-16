0d95537fdbfa9cc098acf0a0738209d0b54946fd
Kat Huang <khuang129@gmail.com>
2021-06-16 12:34:06 -0700

    added error handling

--- routes.js
+++ routes.js
@@ -18,14 +18,23 @@ router.get("/", async function (req, res, next) {
 
 router.get("/:name", async function (req, res, next) {
   const name = req.params.name;
-  const user = await User.getUserByName(name);
+  const usersArr = await User.getUsersArray();
+  const user = usersArr.find((user) => user.name === name);
+  if (!user) {
+    throw new NotFoundError("User does not exist.");
+  }
   return res.json({ user });
 });
 
 router.patch("/:name", async function (req, res, next) {
   const prevName = req.params.name;
   const newName = req.body.name;
-  await User.editUser(prevName, newName);
+  const usersArr = await User.getUsersArray();
+  const idx = usersArr.findIndex((user) => user.name === prevName);
+  if (idx === -1) {
+    throw new NotFoundError("User does not exist");
+  }
+  await User.editUser(idx, newName);
   return res.json({ changed_from: prevName, changed_to: newName });
 });
 
@@ -34,7 +43,7 @@ router.delete("/:name", async function (req, res, next) {
   const name = req.params.name;
   const idx = usersArr.findIndex((user) => user.name === name);
   if (idx === -1) {
-    throw new NotFoundError();
+    throw new NotFoundError("User does not exist");
   }
   await User.deleteUser(idx);
   return res.json({ deleted: name });
--- userMethods.js
+++ userMethods.js
@@ -1,5 +1,4 @@
 const fsP = require("fs").promises;
-const { NotFoundError } = require("./errorHandling");
 
 class User {
   static async getUsersArray() {
@@ -20,15 +19,8 @@ class User {
     }
   }
 
-  static async getUserByName(name) {
+  static async editUser(idx, newName) {
     const usersArr = await this.getUsersArray();
-    console.log(usersArr);
-    return usersArr.find((user) => user.name === name);
-  }
-
-  static async editUser(prevName, newName) {
-    const usersArr = await this.getUsersArray();
-    const idx = usersArr.findIndex((user) => user.name === prevName);
     usersArr[idx].name = newName;
     const usersJSON = JSON.stringify({ users: usersArr });
     try {
